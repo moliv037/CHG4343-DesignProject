32c2a8544e3565158e60dabe97385240ceed706c
Marie-Eve <moliv037@uottawa.ca>
2024-11-09 23:25:17 -0500

    My changes

--- src/StaticParameters.java
+++ src/StaticParameters.java
@@ -23,6 +23,51 @@ public class StaticParameters {
     private double [] theta_reactants;
     private double [] theta_products;
 
+    public StaticParameters(int numberReactants, int numberProducts, int numberInerts, double CA_0, double FA_0, double epsilon) {
+        if (numberReactants <= 0 || numberProducts <= 0 || numberInerts < 0) System.exit(0);
+        this.numberReactants = numberReactants;
+        this.numberProducts = numberProducts;
+        this.numberInerts = numberInerts;
+        if (CA_0 <= 0. || FA_0 <= 0.) System.exit(0);
+        this.CA_0 = CA_0;
+        this.FA_0 = FA_0;
+
+        this.epsilon = epsilon; //epsilon can be 0, negative, or positive
+    }//constructor, need to add throw exceptions
+
+    public StaticParameters(double [] reactantMoleFracs, double [] productMoleFracs, double [] inertMoleFracs) {
+       //check array is not null
+        if(reactantMoleFracs==null) System.exit(0);
+        if(productMoleFracs==null) System.exit(0);
+        if(inertMoleFracs==null) System.exit(0);
+
+        //check that no mole fraction is smaller than 0
+        for (int i = 0; i < reactantMoleFracs.length; i++) {
+            if (reactantMoleFracs[i] < 0.) System.exit(0);}
+        for (int i = 0; i < productMoleFracs.length; i++) {
+            if (productMoleFracs[i] < 0.) System.exit(0);}
+        for (int i = 0; i < inertMoleFracs.length; i++) {
+            if (inertMoleFracs[i] < 0.) System.exit(0);}
+
+        //check that the sum of mole fractions in and out are 1
+        for (int i=0; i < reactantMoleFracs.length; i++) {
+            double sum = 0.0;
+            sum += reactantMoleFracs[i] + inertMoleFracs[i];
+            if (sum != 1.0) System.exit(0);}
+        for (int i = 0; i < productMoleFracs.length; i++) {
+            double sum = 0.0;
+            sum += productMoleFracs[i] + inertMoleFracs[i];
+            if (sum != 1.0) System.exit(0);}
+
+        for (int j=0; j < reactantMoleFracs.length; j++)
+            this.reactantMoleFracs[j]=reactantMoleFracs[j];
+        for (int i=0; i < productMoleFracs.length; i++)
+            this.productMoleFracs[i]=productMoleFracs[i];
+        for (int i=0; i < inertMoleFracs.length; i++)
+            this.inertMoleFracs[i]=inertMoleFracs[i];
+
+    }//constructor, need to add throw exceptions
+
 
 
 
