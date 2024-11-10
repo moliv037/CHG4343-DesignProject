0170575255a6cca959d40d42088fb31c822709aa
EmmaMoore <emoor085@uottawa.ca>
2024-11-10 17:03:27 -0500

    Merge remote-tracking branch 'origin/main'

--- src/AdiabaticPBR.java
+++ src/AdiabaticPBR.java
@@ -72,7 +72,7 @@ public class AdiabaticPBR extends ReactorType implements ODERHS {
                 return -1. * super.returnRateLaw(X, P, T) / super.getParameters().getFA_0();
             //parameter list in calculateRate will need to be updated once that method is defined
             case 1: // dP/dW
-                return -1 / 2 * super.getInput().getAlpha() * (super.getInput().getP0() / (P / super.getInput().getP0())) * (1 + super.getParameters().getEpsilon() * X);
+                return -1. / 2. * super.getInput().getAlpha() * (super.getInput().getP0() / (P / super.getInput().getP0())) * (1 + super.getParameters().getEpsilon() * X);
             case 2: // dT/dW
                 return (-1. * super.returnRateLaw(X, P, T) * super.getInput().getDelH_rx()) / super.returnFiCpi(X);
             default:
