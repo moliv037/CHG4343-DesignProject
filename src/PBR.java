cb2ea477db72c73dcd00986ea1a23051a18ea9b5
EmmaMoore <emoor085@uottawa.ca>
2024-11-11 14:56:22 -0500

    Merge remote-tracking branch 'origin/main'

--- src/PBR.java
+++ src/PBR.java
@@ -1,3 +1,33 @@
 public class PBR {
 
+    private ReactorType reactorType;
+
+    //constructor, copy constructor, clone, accessor, mutator, and equals methods
+    public PBR (ReactorType reactorType) {
+        if (reactorType == null) System.exit(0);
+        this.reactorType = reactorType.clone();
+    }
+    public PBR (PBR source) {
+        if (source == null) System.exit(0);
+        this.reactorType = source.reactorType.clone();
+    }
+    public PBR clone() {
+        return new PBR(this);
+    }
+    public boolean setReactorType (ReactorType reactorType) {
+        if (reactorType == null) return false;
+        this.reactorType = reactorType.clone();
+        return true;
+    }
+    public ReactorType getReactorType () {
+        return reactorType.clone();
+    }
+    public boolean equals (Object comparator) {
+        if (comparator == null) return false;
+        if (comparator.getClass() != this.getClass()) return false;
+        boolean isEquals=true;
+        if (!((PBR)comparator).reactorType.equals(this.reactorType)) isEquals=false;
+        return isEquals;
+    }
+
 }
